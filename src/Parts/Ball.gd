2a033aeb55d60e380f26f0af03d6a920a7f0cf65
kaithewolf <xtremekai27@gmail.com>
2021-09-05 11:26:05 -0700

    incomplete portal fix

--- src/Parts/Ball.gd
+++ src/Parts/Ball.gd
@@ -22,22 +22,3 @@ func _integrate_forces(state):
 	if xform.origin.y < 0:
 		xform.origin.y = autodelete_y
 	state.set_transform(xform)
-
-
-func teleport(area):
-	for portal in get_tree().get_nodes_in_group("portal"):
-		if(portal != area):
-			if(portal.id == area.id):
-				if(!portal.lock_portal):
-					area.do_lock()
-					global_position = portal.global_position
-					
-func _on_hitbox_area_entered(area):
-		if(area.is_in_group("portal")):
-			if(!area.lock_portal):
-				teleport(area)
-
-#func _on_hitbox_body_entered(body):
-#	if(body.is_in_group("portal")):
-#			if(!body.lock_portal):
-#				teleport(body)
--- src/Parts/Portal.gd
+++ src/Parts/Portal.gd
@@ -1,10 +1,42 @@
 extends Area2D
 
 export(int) var id = 0
-
+onready var shape = get_node("CollisionShape2D")
 var lock_portal = false
+var exit_portal = false
+
 
 func do_lock():
 	lock_portal = true
-	yield(get_tree().create_timer(0.5), "timeout")
+	shape.set_deferred("disabled", true)
+
+func unlock():
 	lock_portal = false
+	shape.set_deferred("disabled", false)
+	
+
+
+func _on_Portal_body_entered(body):
+	# if self can be entered,
+	# teleport to portal after self
+	if !exit_portal:
+		print("teleported")
+		var go_one_more = false
+		for portal in get_tree().get_nodes_in_group("portal"):
+			if go_one_more:
+					go_one_more = false
+					portal.exit_portal = true
+					body.set_position(portal.position)
+					print("position moved")
+			if portal == self:
+				go_one_more = true
+			
+	
+
+
+func _on_Portal_body_exited(body):
+	#exiting from exit portal re-enables TP for that portal
+	if exit_portal == true:
+		print("exited")
+		for portal in get_tree().get_nodes_in_group("portal"):
+			exit_portal = false
