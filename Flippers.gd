fb116457c0077c005f7f2e59351be1eb7764c3ec
kaithewolf <xtremekai27@gmail.com>
2021-09-05 14:46:38 -0700

    Added flippersL and R

---
+++ Flippers.gd
@@ -0,0 +1,12 @@
+extends RigidBody2D
+onready var TweenNode = get_node("Tween")
+
+func _process(delta):
+	if Input.is_action_pressed("FlipperL"):
+		TweenNode.interpolate_property(self, "rotation_degrees", rotation_degrees, -70, 0.06, Tween.TRANS_LINEAR, Tween.EASE_IN, 0)
+		TweenNode.start()
+	if Input.is_action_just_released("FlipperL"):
+		TweenNode.stop(self)
+		TweenNode.interpolate_property(self, "rotation_degrees", rotation_degrees, 0, 0.11, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0)
+		TweenNode.start()
+
---
+++ src/Parts/FlipperR.gd
@@ -0,0 +1,11 @@
+extends RigidBody2D
+onready var TweenNode = get_node("Tween")
+
+func _process(delta):
+	if Input.is_action_pressed("FlipperR"):
+		TweenNode.interpolate_property(self, "rotation_degrees", rotation_degrees, 70, 0.06, Tween.TRANS_LINEAR, Tween.EASE_IN, 0)
+		TweenNode.start()
+	if Input.is_action_just_released("FlipperR"):
+		TweenNode.stop(self)
+		TweenNode.interpolate_property(self, "rotation_degrees", rotation_degrees, 0, 0.11, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0)
+		TweenNode.start()
