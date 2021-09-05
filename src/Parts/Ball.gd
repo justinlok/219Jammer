3221684efb0b627130e121b30f9e8c24efea13c4
radi3402 <bobsmithusername66@gmail.com>
2021-09-04 23:14:18 -0700

    Portal

--- src/Parts/Ball.gd
+++ src/Parts/Ball.gd
@@ -11,6 +11,9 @@ onready var autodelete_y : float = OS.window_size.y
 #		else:
 #			self.queue_free()
 
+#portal linkage id
+var portal_id = 0
+
 func _integrate_forces(state):
 	var xform = state.get_transform()
 	if xform.origin.y > autodelete_y:
@@ -18,3 +21,17 @@ func _integrate_forces(state):
 	if xform.origin.y < 0:
 		xform.origin.y = autodelete_y
 	state.set_transform(xform)
+
+
+func _on_Ball_area_entered(area):
+	if (area.is_in_group("portal")):
+		if (!area.lock_portal):
+			do_teleport(area)
+
+func do_teleport(area):
+	for portal in get_tree().get_nodes_in_group("portal"):
+		if(portal != area):
+			if(portal.id == area.id):
+				if(!portal.lock_portal):
+					area.do_lock()
+					global_position = portal.global_position
