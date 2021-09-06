321c9a79c998372bf370a8dcc9719ac298e2debd
Justin Lok <32407493+justinlok@users.noreply.github.com>
2021-09-05 19:32:34 -0700

    Delete part indicator.

--- src/UserInterface/DeleteShape.gd
+++ src/UserInterface/DeleteShape.gd
@@ -5,8 +5,10 @@ var is_delete_mode : bool = false
 func _input(event):
 	if event.is_action_pressed("editor_delete"):
 		is_delete_mode = true
+		self.visible = true
 	if event.is_action_released("editor_delete"):
 		is_delete_mode = false
+		self.visible = false
 
 
 func _on_DeleteShape_input_event(viewport, event, shape_idx):
