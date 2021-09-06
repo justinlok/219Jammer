3a800d3ef2e4046de6b92e827de01f6732739848
kaithewolf <xtremekai27@gmail.com>
2021-09-06 10:25:10 -0700

    new bumper images and launcher resized

--- src/Parts/Launcher.gd
+++ src/Parts/Launcher.gd
@@ -2,14 +2,15 @@ extends Node2D
 
 onready var elevator = get_node("Elevator")
 onready var anim_player = get_node("Elevator/AnimationPlayer")
-onready var burst = get_node("Burst")
+onready var burst = get_node("Ball/Burst")
 onready var tweenNode = get_node("Elevator/Tween")
 func _ready():
 	elevator.frame = 0
-	elevator.position = Vector2(-72.29, -400)
+	elevator.position = Vector2(-30.32, -84.731)
+	burst.visible = false
 	play_entrance()
 
 func play_entrance():
-	tweenNode.interpolate_property(elevator, "position:y", -400, 546, 5, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0)
+	tweenNode.interpolate_property(elevator, "position:y", -84.731, 329.613, 5, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0)
 	anim_player.play("crystal")
 	tweenNode.start()
