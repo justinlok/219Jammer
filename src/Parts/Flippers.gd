8d624019aa417f6b35f7ffd1f30cfd9222f49a73
kaithewolf <xtremekai27@gmail.com>
2021-09-06 08:51:57 -0700

    shaking_thing works and has sound
    also rearranged some bumper files into src/Parts, instead of res://

--- src/Parts/Flippers.gd
+++ src/Parts/Flippers.gd
@@ -4,7 +4,7 @@ onready var Init_rotation = self.rotation_degrees
 
 func _process(delta):
 	if Input.is_action_pressed("FlipperL"):
-		TweenNode.interpolate_property(self, "rotation_degrees", null, Init_rotation-70, 0.06, Tween.TRANS_LINEAR, Tween.EASE_IN, 0)
+		TweenNode.interpolate_property(self, "rotation_degrees", null, Init_rotation-70, 0.05, Tween.TRANS_LINEAR, Tween.EASE_IN, 0)
 		TweenNode.start()
 	if Input.is_action_just_released("FlipperL"):
 		TweenNode.stop(self)
