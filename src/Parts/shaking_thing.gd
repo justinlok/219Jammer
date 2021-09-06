0f52d611eb9dcce2bf2a63689a47c1b2cbdacce1
Justin Lok <32407493+justinlok@users.noreply.github.com>
2021-09-06 12:47:00 -0700

    Update shaking_thing.gd

--- src/Parts/shaking_thing.gd
+++ src/Parts/shaking_thing.gd
@@ -9,7 +9,8 @@ func _ready():
 
 func _on_Bumper_box_body_entered(body):
 	if bound_check.overlaps_body(body):
-		var flip = self.scale.x/abs(self.scale.x) #account for flipping horizontally
-		var direction = Vector2(cos(deg2rad(rotation_degrees+flip*61)),sin(deg2rad(rotation_degrees+flip*61)))
-		audio.play()
-		body.set_linear_velocity(10*bumper_strength*direction.normalized())
+		if body.get_name() == "Ball":
+			var flip = self.scale.x/abs(self.scale.x) #account for flipping horizontally
+			var direction = Vector2(cos(deg2rad(rotation_degrees+flip*61)),sin(deg2rad(rotation_degrees+flip*61)))
+			audio.play()
+			body.set_linear_velocity(10*bumper_strength*direction.normalized())
