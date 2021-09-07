23151fbb8dc3174a5ecdae97f8955b895deff808
kaithewolf <xtremekai27@gmail.com>
2021-09-07 07:04:44 -0700

    added superdash sprite for firing animation

--- src/Parts/Launcher.gd
+++ src/Parts/Launcher.gd
@@ -5,6 +5,7 @@ export var elevator_length : float = 220
 onready var animation_player : AnimationPlayer = get_node("Elevator/AnimationPlayer")
 onready var tween : Tween = get_node("Elevator/Tween")
 
+
 var input_lock : bool = true
 
 func _ready():
@@ -32,10 +33,18 @@ func _input(event):
 	if event.is_action("Launch"):
 		#for ball in balls:
 		#	ball.set_linear_velocity(Vector2(0,-1000))
+		var burst_tex = load("res://assets/sprites/burst.png")
+		$Elevator.set_texture(burst_tex)
+		$Elevator.hframes = 1
+		$Elevator.frame = 0
 		_play_fire()
 
 
 func _on_Tween_tween_all_completed():
 	input_lock = !input_lock
 	if input_lock:
+		var elevator_tex = load("res://assets/sprites/Elevator_sheet.png")
+		$Elevator.set_texture(elevator_tex)
+		$Elevator.hframes = 6
+		$Elevator.frame = 0
 		_play_reload()
