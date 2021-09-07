e49d6a746d863b805a51e7c3d500624e3d3ef073
Justin Lok <32407493+justinlok@users.noreply.github.com>
2021-09-06 19:59:41 -0700

    Cleaned up code.

--- bumper.gd
+++ bumper.gd
@@ -2,26 +2,23 @@ extends Area2D
 export (int) var bumper_strength
 export (int) var score
 
-onready var sprite = get_node("Sprite")
-onready var tween = get_node("Tween")
-onready var big = sprite.scale*1.5
-onready var small = sprite.duplicate(true)
-onready var player = get_node("AudioStreamPlayer2D")
-func light_up():
-	
-	tween.interpolate_property(sprite, "scale", big, small.scale, 0.1, Tween.TRANS_ELASTIC, Tween.EASE_OUT, 0)
-	tween.start()
-	player.play()
+onready var big : Vector2 = $Sprite.scale*1.5
+onready var small : Sprite = $Sprite.duplicate(true)
+
+func _light_up():
+	$Tween.interpolate_property($Sprite, "scale", big, small.scale, 0.1, $Tween.TRANS_ELASTIC, $Tween.EASE_OUT, 0)
+	$Tween.start()
+	$AudioStreamPlayer2D.play()
 
 
 func _on_Bumper_body_entered(body):
 	var new_angle = body.global_position - self.global_position
-	small = sprite.duplicate(true)
-	big = sprite.scale*1.5
+	small = $Sprite.duplicate(true)
+	big = $Sprite.scale*1.5
 	var body_speed = sqrt(pow(body.get_linear_velocity().x,2)+pow(body.get_linear_velocity().y,2))
 	if body_speed > bumper_strength*10:
 		body.set_linear_velocity(body_speed*new_angle.normalized())
 	else:
 		body.set_linear_velocity(10*bumper_strength*new_angle.normalized())
-	
-	light_up()
+
+	_light_up()
--- src/UserInterface/PartButton.gd
+++ src/UserInterface/PartButton.gd
@@ -2,6 +2,5 @@ extends Button
 
 var path : String = ""
 
-
 func _on_PartButton_pressed():
 	get_parent().set_selected_part(path)
